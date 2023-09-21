07119d0a9659c27a2691146a158603c022bb0cca
zin <88925213+zzzzzzin@users.noreply.github.com>
2023-09-21 16:16:38 +0900

    Update yujin.py

---
+++ socrates_python100/51to60/yujin.py
@@ -0,0 +1,80 @@
+#51
+print("첫번째 값을 입력하시요: ")
+a = input()
+
+print("두번째 값을 입력하시요: ")
+b = input()
+
+print("순서대로 "+a+", "+b+" 입니다")
+
+if a == b:
+    print("TRUE")
+else:
+    print("FALSE")
+
+#52
+print("값을 입력하시요: ")
+a = bool(int(input()))
+print(not a)
+
+#53
+print("값을 입력하시요: ")
+a = bool(int(input()))
+print(a)
+
+#54
+print("두개의 정수를 띄어서 입력하시요: ")
+a, b=input().split()
+
+x=bool(int(a))
+y=bool(int(b))
+
+if x and y == True:
+  print(True)
+else: print(False)
+
+#55
+print("두개의 정수를 띄어서 입력하시요: ")
+a, b=input().split()
+
+x=bool(int(a))
+y=bool(int(b))
+
+if x or y == True:
+  print(True)
+else: print(False)
+
+#56
+print("두개의 정수를 띄어서 입력하시요: ")
+a, b = input().split()
+
+x = bool(int(a))
+y = bool(int(b))
+
+z = x ^ y
+print(z)
+
+#57
+print("두개의 정수를 띄어서 입력하시요: ")
+a, b = input().split()
+
+x = bool(int(a))
+y = bool(int(b))
+
+z = x ^ y
+print(not z)
+
+#58
+print("두개의 정수를 띄어서 입력하시요: ")
+a, b = input().split()
+
+x = bool(int(a))
+y = bool(int(b))
+
+z = not x and not y
+print(z)
+
+#59
+
+
+
