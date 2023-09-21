9dbe87a4e24397ed48ceeab716f83d719e7a10b0
zin <88925213+zzzzzzin@users.noreply.github.com>
2023-09-21 16:25:41 +0900

    Update yujin.py

--- socrates_python100/51to60/yujin.py
+++ socrates_python100/51to60/yujin.py
@@ -1,8 +1,8 @@
 #51
-print("첫번째 값을 입력하시요: ")
+print("첫번째 값을 입력하시오: ")
 a = input()
 
-print("두번째 값을 입력하시요: ")
+print("두번째 값을 입력하시오: ")
 b = input()
 
 print("순서대로 "+a+", "+b+" 입니다")
@@ -13,17 +13,17 @@ else:
     print("FALSE")
 
 #52
-print("값을 입력하시요: ")
+print("값을 입력하시오: ")
 a = bool(int(input()))
 print(not a)
 
 #53
-print("값을 입력하시요: ")
+print("값을 입력하시오: ")
 a = bool(int(input()))
 print(a)
 
 #54
-print("두개의 정수를 띄어서 입력하시요: ")
+print("두개의 정수를 띄어서 입력하시오: ")
 a, b=input().split()
 
 x=bool(int(a))
@@ -34,7 +34,7 @@ if x and y == True:
 else: print(False)
 
 #55
-print("두개의 정수를 띄어서 입력하시요: ")
+print("두개의 정수를 띄어서 입력하시오: ")
 a, b=input().split()
 
 x=bool(int(a))
@@ -45,7 +45,7 @@ if x or y == True:
 else: print(False)
 
 #56
-print("두개의 정수를 띄어서 입력하시요: ")
+print("두개의 정수를 띄어서 입력하시오: ")
 a, b = input().split()
 
 x = bool(int(a))
@@ -55,7 +55,7 @@ z = x ^ y
 print(z)
 
 #57
-print("두개의 정수를 띄어서 입력하시요: ")
+print("두개의 정수를 띄어서 입력하시오: ")
 a, b = input().split()
 
 x = bool(int(a))
@@ -65,7 +65,7 @@ z = x ^ y
 print(not z)
 
 #58
-print("두개의 정수를 띄어서 입력하시요: ")
+print("두개의 정수를 띄어서 입력하시오: ")
 a, b = input().split()
 
 x = bool(int(a))
@@ -75,6 +75,10 @@ z = not x and not y
 print(z)
 
 #59
+print("하나의 정수를 입력하시오: ")
+a = int(input())
+x = ~a
+print(x)
 
-
+#60
 
