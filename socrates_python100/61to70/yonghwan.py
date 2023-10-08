beadcdf2a7e5b1921cd112f97ef05a20d403c521
YongHwan CHO <64125970+cho16147@users.noreply.github.com>
2023-10-08 21:08:28 +0900

    Update yonghwan.py

--- socrates_python100/61to70/yonghwan.py
+++ socrates_python100/61to70/yonghwan.py
@@ -34,3 +34,81 @@ if b%2==0 :
   print(b) 
 if c%2==0 :
   print(c)
+
+# 문제66번
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+if a%2==0 :
+    print("even")
+else :
+    print("odd")
+if b%2==0 :
+    print("even")
+else :
+    print("odd")
+if c%2==0 :
+    print("even")
+else :
+    print("odd")
+
+# 문제67번
+a = input()
+a = int(a)
+if a<0 :
+  if a%2==0 :
+    print('A')
+  else :
+    print('B')
+else :
+  if a%2==0 :
+    print('C')
+  else :
+    print('D')
+
+# 문제68번
+a = input()
+a = int(a)
+
+if 90 <= a <= 100:
+    print("A")
+else:
+    if a >= 70:
+        print("B")
+    else:
+        if a >= 40:
+            print("C")
+        else:
+            print("D")
+
+# 문제69번
+a = input()
+a = str(a)
+if a == "A":
+    print("best!!!")
+else:
+    if a == "B":
+        print("good!!")
+    else:
+        if a == "C":
+            print("run!")
+        else:
+            if a == "D":
+                print("slowly~")
+            else:
+                print("what?")
+
+# 문제70번
+a = input()
+a = int(a)
+if 3<=a<=5:
+    print("spring")
+else:
+    if 6<=a<=8:
+        print("summer")
+    else:
+        if 9<=a<=11:
+            print("fall")
+        else:
+            print("winter")
